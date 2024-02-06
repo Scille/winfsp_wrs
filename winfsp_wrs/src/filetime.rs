3369d55435f932f0e4749c4ac97882b0d033ef0f
TimeEngineer <chenpascal241096@yahoo.fr>
2024-02-06 18:08:20 +0100

    Update dependencies

--- winfsp_wrs/src/filetime.rs
+++ winfsp_wrs/src/filetime.rs
@@ -5,9 +5,12 @@ use chrono::{DateTime, Utc};
 const EPOCH_AS_FILETIME: u64 = 116444736000000000; // January 1, 1970 as MS file time
 
 pub fn filetime_now() -> u64 {
-    Utc::now().timestamp_nanos() as u64 / 100 + EPOCH_AS_FILETIME
+    Utc::now().timestamp_nanos_opt().expect("Year 2262 reached") as u64 / 100 + EPOCH_AS_FILETIME
 }
 
 pub fn filetime_from_utc(dt: DateTime<Utc>) -> u64 {
-    dt.timestamp_nanos() as u64 / 100 + EPOCH_AS_FILETIME
+    dt.timestamp_nanos_opt()
+        .expect("Datetime reached year 2262, cannot convert to nano seconds") as u64
+        / 100
+        + EPOCH_AS_FILETIME
 }

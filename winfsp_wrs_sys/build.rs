12eeff01a934427eb5e02764baee3c461ff19a00
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-08-07 09:16:25 +0200

    Allow compilation of `winfsp_wrs_sys` on non-Windows platforms
    Compilation on non-Windows platform will obviously fail (as WinFSP is only available
    on Windows).
    However keeping the Rust part platform agnostic is still useful given it allows
    linter & IDE to work correctly when groking into the code from a non-Windows machine.

--- winfsp_wrs_sys/build.rs
+++ winfsp_wrs_sys/build.rs
@@ -1,22 +1,18 @@
-use std::path::PathBuf;
-
-fn get_winfsp_install_dir() -> PathBuf {
+#[cfg(windows)]
+fn get_winfsp_install_dir() -> std::path::PathBuf {
     let winfsp_install = registry::Hive::LocalMachine
         .open("SOFTWARE\\WOW6432Node\\WinFsp", registry::Security::Read)
         .ok()
         .and_then(|u| u.value("InstallDir").ok())
         .expect("WinFsp installation directory not found.");
     match winfsp_install {
-        registry::Data::String(path) => PathBuf::from(path.to_os_string()),
+        registry::Data::String(path) => std::path::PathBuf::from(path.to_os_string()),
         _ => panic!("unexpected install directory"),
     }
 }
 
+#[cfg(windows)]
 fn main() {
-    if !cfg!(windows) {
-        panic!("WinFSP is only supported on Windows.");
-    }
-
     let winfsp_install_dir = get_winfsp_install_dir();
     println!(
         "cargo:rustc-link-search={}/lib",
@@ -37,3 +33,10 @@ fn main() {
         panic!("unsupported triple {}", std::env::var("TARGET").unwrap())
     };
 }
+
+// Compilation on non-Windows platform will obviously fail (as WinFSP is only available
+// on Windows).
+// However keeping the Rust part platform agnostic is still useful given it allows
+// linter & IDE to work correctly when groking into the code from a non-Windows machine.
+#[cfg(not(windows))]
+fn main() {}
